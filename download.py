d931831f09ae793963a3e1eebd83cca98f8d05ce
LiquoriceDerg <licoqi2@gmail.com>
2023-06-25 22:31:07 +0800

    Update download.py

--- download.py
+++ download.py
@@ -26,7 +26,8 @@ parser.add_argument('-c', '--chat', type=str,
                     help='Specify a chat name like "https://t.me/xxx1234" or just "xxx1234". ')
 parser.add_argument('-l', '--limit-size', type=int, metavar=100,
                     help='Specify a file size limit(MB), default to 100, files larger than this value will be skipped.')
-parser.add_argument('-r', '--reverse-order', action='store_true', help='Enable download from new to old. Some servers will put you into floodwait if you try to access some of the oldest chat messages.')
+parser.add_argument('-r', '--reverse-order', action='store_true',
+                    help='Enable download from new to old. Some servers will put you into floodwait if you try to access some of the oldest chat messages.')
 args = parser.parse_args()
 
 
@@ -35,6 +36,7 @@ if C.api_hash == "your_api_hash_here":
     sys.exit(1)
 
 media_types = ["video", "photo", "document"]
+# media_types = [ "document"]
 media_suffix = {
     "video": "mp4",
     "photo": "jpg",
@@ -46,7 +48,7 @@ if os.name == 'nt':
 else:
     path_divider = "/"
 
-message_batch_size = 10
+message_batch_size = 50
 quitting = False
 
 if args.chat:
@@ -62,6 +64,14 @@ if args.limit_size:
 else:
     limit_size = 100 * 1024**2
 
+if path.exists(path.join(".", chat_id, "progress.json")):
+    with open(path.join(".", chat_id, "progress.json"), 'r') as f:
+        data = json.loads(f.read())
+        offset_id = data["offset_id"]
+        args.reverse_order = data["reverse"]
+else:
+    offset_id = 1 + message_batch_size
+
 
 def progress_bar(current, total):
     p = current/total
@@ -70,35 +80,41 @@ def progress_bar(current, total):
     sys.stdout.flush()
 
 
+def log(txt):
+    global chat_id
+    with open(path.join(".", chat_id, "log.txt"), "a") as f:
+        f.write(txt+"\n"+20*'-'+'\n')
+    sys.stdout.write(txt+"\n"+20*'-'+'\n')
+    sys.stdout.flush()
+
+
 async def main():
-    global quitting
+    global quitting, offset_id
     if not path.exists(chat_id):
         mkdir(chat_id)
     async with Client("my_account", C.api_id, C.api_hash, sleep_threshold=6000) as app:
 
         chat_history_count = await app.get_chat_history_count(chat_id)
         print(chat_history_count)
-        if path.exists(path.join(".", chat_id, "progress.json")):
-            with open(path.join(".", chat_id, "progress.json"), 'r') as f:
-                data = json.loads(f.read())
-                offset_id = data["offset_id"]
-                args.reverse_order = data["reverse"]
-        else:
-            offset_id = 1 + message_batch_size
+
         while offset_id < chat_history_count:
             cnt = 0
             try:
                 if args.reverse_order:
-                    gen = app.get_chat_history(chat_id, limit=message_batch_size, offset=offset_id-message_batch_size-1)
+                    gen = app.get_chat_history(
+                        chat_id, limit=message_batch_size, offset=offset_id-message_batch_size-1)
                 else:
-                    gen = app.get_chat_history(chat_id, limit=message_batch_size, offset_id=offset_id)
+                    gen = app.get_chat_history(
+                        chat_id, limit=message_batch_size, offset_id=offset_id)
                 async for message in gen:
                     # print(message)
                     if (quitting):
                         break
                     # print("Processing message",message.id,'...')
-                    if message.from_user:
-                        print("{}:[{}]{}".format(
+                    if message.from_user and message.text:
+                        # print("{}:[{}]{}".format(
+                        #     message.from_user.username, message.id, message.text))
+                        log("{}:[{}]{}".format(
                             message.from_user.username, message.id, message.text))
                     for media_type in media_types:
                         if message.__getattribute__(media_type):
@@ -114,12 +130,12 @@ async def main():
                                 filename = path.join(
                                     '.', chat_id, media_type, filename)
                             if media.file_size > limit_size:
-                                print("[{}/{}]".format(offset_id-message_batch_size +
-                                                       cnt, chat_history_count), filename, "too large({:.2f}MB), skipped.".format(media.file_size/1024**2) )
+                                log("[{}/{}] {} too large({:.2f}MB), skipped.".format(offset_id-message_batch_size +
+                                                                                      cnt, chat_history_count, filename, media.file_size/1024**2))
                                 continue
                             if path.exists(filename):
-                                print("[{}/{}]".format(offset_id-message_batch_size +
-                                                       cnt, chat_history_count), filename, "already exsits, skipped.")
+                                log("[{}/{}] {} already exsits, skipped.".format(offset_id-message_batch_size +
+                                                                                 cnt, chat_history_count, filename))
                                 continue
                             print(
                                 "[{}/{}]".format(offset_id-message_batch_size+cnt, chat_history_count), filename)
@@ -129,6 +145,8 @@ async def main():
                                 quitting = True
                                 break
                             print()
+                            log("[{}/{}] {} done.".format(offset_id -
+                                message_batch_size+cnt, chat_history_count, filename))
                     cnt += 1
             except FloodWait as e:
                 print(e)
